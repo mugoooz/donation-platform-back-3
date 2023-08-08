a285f3ba5b21551540123ef0b53ec63e49ad2ff7
Eric Mugo <mugoericpg20@gmail.com>
2023-08-08 16:58:04 +0300

    models & controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :donors
+  resources :donations
+  resources :charities
+  resources :beneficiaries
+  resources :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
---
+++ db/migrate/20230808135710_create_donors.rb
@@ -0,0 +1,8 @@
+class CreateDonors < ActiveRecord::Migration[7.0]
+  def change
+    create_table :donors do |t|
+
+      t.timestamps
+    end
+  end
+end

55eada5f39af2908f12d38e030e9e7355bf41c85
Eric Mugo <mugoericpg20@gmail.com>
2023-08-10 12:12:33 +0300

    migrations(charity & donor fix)

--- db/migrate/20230808135701_create_donations.rb
+++ db/migrate/20230808135701_create_donations.rb
@@ -1,15 +1,15 @@
 class CreateDonations < ActiveRecord::Migration[7.0]
   def change
     create_table :donations do |t|
-      t.integer :charity_id
-      t.integer :donor_id
+      t.string :charity
+      t.string :donor
       t.decimal :amount
       t.string :name
       t.string :location
       t.string :phone_number
       t.boolean :anonymous
       t.string :recurrence
-      t.string :stripe_session_id
+      
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -31,15 +31,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_08_135710) do
   end
 
   create_table "donations", force: :cascade do |t|
-    t.integer "charity_id"
-    t.integer "donor_id"
+    t.string "charity"
+    t.string "donor"
     t.decimal "amount"
     t.string "name"
     t.string "location"
     t.string "phone_number"
     t.boolean "anonymous"
     t.string "recurrence"
-    t.string "stripe_session_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
